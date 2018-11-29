1a6784a29ac2babff08af32046700d7ffb564608
= <voistinoff.vladimir@yandex.ru>
2018-11-30 02:01:31 +0300

    moved Context

--- context.go
+++ context.go
@@ -1,5 +1,11 @@
 package cheky
 
+// Context keeps query and path parameters.
+type Context interface {
+	Query(name string) string
+	Path(name string) string
+}
+
 // Ctx returns new QPBuilder from a given Context.
 func Ctx(ctx Context) QPBuilder {
 	return &QueryPath{ctx: ctx}
