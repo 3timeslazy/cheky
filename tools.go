e4edc842f96991a781a7b42df421144be7b7ad2f
= <voistinoff.vladimir@yandex.ru>
2018-12-06 02:17:25 +0300

    updated tools

--- tools.go
+++ tools.go
@@ -181,7 +181,7 @@ func lt(val, other interface{}) (err error) {
 		}
 	}
 
-	return err
+	return
 }
 
 func le(val, other interface{}) (err error) {
@@ -238,6 +238,72 @@ func le(val, other interface{}) (err error) {
 	case float64:
 		if v <= other.(float64) {
 			err = nil
+		}
+	}
+
+	return
+}
+
+func oneOf(val, ones interface{}) error {
+	return enum(val, ones, true)
+}
+
+func noOne(val, ones interface{}) error {
+	return enum(val, ones, false)
+}
+
+func enum(val, ones interface{}, oneOf bool) (err error) {
+	errUpd := func(isEqual bool) {
+		if isEqual {
+			err = fmt.Errorf(
+				"value should not be one of %v, got '%v'",
+				ones, val,
+			)
+		}
+	}
+
+	if oneOf {
+		err = fmt.Errorf(
+			"value should be one of %v, got '%v'",
+			ones, val,
+		)
+		errUpd = func(isEqual bool) {
+			if isEqual {
+				err = nil
+			}
+		}
+	}
+
+	switch ones := ones.(type) {
+	case []string:
+		val := val.(string)
+		for _, one := range ones {
+			errUpd(val == one)
+		}
+	case []int:
+		val := val.(int)
+		for _, one := range ones {
+			errUpd(val == one)
+		}
+	case []int8:
+		val := val.(int8)
+		for _, one := range ones {
+			errUpd(val == one)
+		}
+	case []int16:
+		val := val.(int16)
+		for _, one := range ones {
+			errUpd(val == one)
+		}
+	case []int32:
+		val := val.(int32)
+		for _, one := range ones {
+			errUpd(val == one)
+		}
+	case []int64:
+		val := val.(int64)
+		for _, one := range ones {
+			errUpd(val == one)
 		}
 	}
 
